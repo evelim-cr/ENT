4e0b1d28c963ff38b0dc090b9e7b09121a365594
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2015-08-25 17:17:10 -0300

    Add parallax effect

---
+++ src/app/components/parallax/parallax.directive.js
@@ -0,0 +1,63 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ent')
+    .directive('entParallax', entParallax);
+
+  /** @ngInject */
+  function entParallax($window) {
+    var directive = {
+      restrict: 'A',
+      scope: {
+        parallaxCss: '@',
+        parallaxInitVal: '@',
+        parallaxRatio: '@'
+      },
+      link: linkFunc
+    };
+
+    function linkFunc(scope, element, attrs) {
+      var cssKey,
+          cssValue,
+          isSpecialVal,
+          parallaxCssVal,
+          parallaxOffset,
+          parallaxRatio,
+          parallaxInitVal,
+          cssValArray;
+
+      parallaxCssVal = scope.parallaxCss ? scope.parallaxCss : 'top';
+      cssValArray = parallaxCssVal.split(':');
+      cssKey = cssValArray[0];
+      cssValue = cssValArray[1];
+
+      isSpecialVal = cssValue ? true : false;
+      if (!cssValue) {
+        cssValue = cssKey;
+      }
+
+      parallaxRatio = scope.parallaxRatio ? +scope.parallaxRatio : 1.1;
+      parallaxInitVal = scope.parallaxInitVal ? +scope.parallaxInitVal : 0;
+
+      element.css(cssKey, parallaxInitVal + 'px');
+
+      function _onScroll() {
+        var resultVal;
+        var calcVal = $window.pageYOffset * parallaxRatio + parallaxInitVal;
+
+        if (isSpecialVal) {
+          resultVal = '' + cssValue + '(' + calcVal + 'px)';
+        } else {
+          resultVal = calcVal + 'px';
+        }
+        element.css(cssKey, resultVal);
+      }
+
+      $window.addEventListener('scroll', _onScroll);
+    }
+
+    return directive;
+  }
+
+})();
